54219b6fca9b51a21729aa89f27e01fc49c2d5a2
Zahra Bayat <58395585+ZahraBayatgh@users.noreply.github.com>
2023-01-28 17:25:49 +0330

    Add project files.

---
+++ PerformanceInEnumerableSample/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerformanceInEnumerableSample
+{
+    public class Person
+    {
+
+        public Person(int id, string name , int age = 0)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ PerformanceInEnumerableSample/Program.cs
@@ -0,0 +1,21 @@
+﻿using PerformanceInEnumerableSample;
+var people = GetPeople().ToList();
+
+var count=people.Count();
+Console.WriteLine($"People data count: {count}");
+
+foreach (var item in people)
+{
+    Console.WriteLine($"Id: {item.Id}, Name: {item.Name}, Age: {item.Age}");
+}
+
+
+IEnumerable<Person> GetPeople()
+{
+    var lines = File.ReadAllLines("./Person.csv");
+    foreach (var item in lines)
+    {
+        var data = item.Split(',');
+        yield return new Person(int.Parse(data[0]), data[1], int.Parse(data[2]));
+    }
+}
